3b71d9d5ca7c48aa158986466c7633d3b0970bea
Kristen Picard <kristenpicard93@gmail.com>
2021-06-14 13:54:15 -0400

    Added header and navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Projects from "./pages/Projects";
-import Navbar from "./components/Footer";
-import Footer from "./components/Navbar";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./App.css"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <Header />
           <Route exact path="/" component={Main} />
           <Route exact path="/projects" component={Projects} />
         <Footer />
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Header() {
+  return (
+    <>
+      
+      <div className="card header bg-dark text-center">
+        <h1 className="display-4 neon">Kristen Picard Portfolio</h1>
+        <br></br>
+        <p className="lead">Welcome! Stay awhile and look around.</p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
