52fbe81b1c5f1c6bd90534dbee37d664b94aa4e2
Kristen Picard <kristenpicard93@gmail.com>
2021-06-14 12:49:22 -0400

    setting up App.js and index.js with router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/projects" component={Projects} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
