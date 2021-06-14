1f7680707334ccbeaa3e48d534f4ec7c4b30ea5e
Kristen Picard <kristenpicard93@gmail.com>
2021-06-14 13:23:19 -0400

    Got router working

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Projects from "./pages/Projects";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Footer";
+import Footer from "./components/Navbar";
 
 function App() {
   return (
