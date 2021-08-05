003ccf6c387fb45f47cec9ae4176129447fa70b5
Kristen Picard <kristenpicard93@gmail.com>
2021-08-05 12:19:18 -0500

    Updating Resume Link

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -40,7 +40,7 @@ function Links() {
             </a>
             <br></br>
             <a
-              href="https://drive.google.com/file/d/157cTwu9KdxpVSoJC5gL_1ORLvBXx_WeU/view?usp=sharing"
+              href="https://drive.google.com/file/d/1n7S3ZnlCKJqvugoD_e54qwg-y0Sy3VPI/view?usp=sharing"
               type="button"
               className="btn btn-outline-secondary btn-lg btn-block col-10"
               target="_blank"
