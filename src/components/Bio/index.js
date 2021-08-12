6152ca33a87d276ac0fad4fd65c575df6ef523a2
Kristen Picard <kristenpicard93@gmail.com>
2021-08-12 12:29:54 -0500

    Linking more projects and editing bio

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -16,14 +16,15 @@ function Bio() {
         <div className="card-body biobody">
           <h5 className="card-title">Biography</h5>
           <p className="card-text">
-            Full Stack web developer utilizing special blend of technical and
-            interpersonal skills to efficiently translate business needs into
-            user-friendly, web functionality. Recently earned a certificate in
-            full stack development from The Ohio State University, with newly
-            developed skills in JavaScript, CSS, React.js, and mobile-first
-            responsive web design. A self-motivated individual desiring to
-            leverage strong time-management skills to efficiently prioritize
-            competing objectives.
+            Full-Stack web developer with a unique blend of technical and
+            interpersonal skills and an execution mindset that will add value to
+            teams and work efficiently to translate business needs into
+            user-friendly software. Graduate of The Ohio State University's
+            Full-Stack Coding Bootcamp, with newly developed skills in
+            JavaScript, CSS, React.js, and mobile-first responsive web design. A
+            self-motivated individual desiring to leverage strong
+            time-management skills to efficiently prioritize competing
+            objectives.
           </p>
         </div>
       </div>
